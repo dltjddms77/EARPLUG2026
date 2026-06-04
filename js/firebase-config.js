b0dd71698263d519e6f995819c374efb5bc06a6e
dltjddms77 <dltjddms77@gmail.com>
2026-06-04 21:11:58 +0900

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,16 +1,15 @@
 // ============================================================
-//  Firebase 설정
-//  Firebase 콘솔 → 프로젝트 설정 → "내 앱"에서 복사한 값으로
-//  아래 항목을 전부 교체하세요. (README의 1~4단계 참고)
+//  Firebase 설정 (EARPLUG 프로젝트 값으로 채워짐)
 // ============================================================
 
 export const firebaseConfig = {
-  apiKey: "여기에_API_KEY",
-  authDomain: "여기에_PROJECT_ID.firebaseapp.com",
-  projectId: "여기에_PROJECT_ID",
-  storageBucket: "여기에_PROJECT_ID.appspot.com",
-  messagingSenderId: "여기에_SENDER_ID",
-  appId: "여기에_APP_ID",
+  apiKey: "AIzaSyDTPIq4hk6vcl4-g3dasb4OGuoD2zLBq2M",
+  authDomain: "earplug2026.firebaseapp.com",
+  projectId: "earplug2026",
+  storageBucket: "earplug2026.firebasestorage.app",
+  messagingSenderId: "465661006332",
+  appId: "1:465661006332:web:e8bcac75700a346c85a279",
+  measurementId: "G-T8LBW55CGG",
 };
 
 // 단일 공연이므로 정보를 한 문서에 저장합니다. (바꿀 필요 없음)
